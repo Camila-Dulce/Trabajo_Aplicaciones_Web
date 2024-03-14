544547e9eca8a6354dedb0842657c2a8dc7130b6
Sala G19 <CSalaG19@uniboyaca.edu.co>
2024-03-13 20:58:47 -0500

    codigo base html y css de juego

---
+++ jUEGO_MEMORIA/juego_javascript/index.js
@@ -0,0 +1,13 @@
+let botons =  document.getElementsByClassName('seleccionar');
+
+let datos=[
+    1,3,6,1,6,3
+];
+
+[...botons].forEach(boton=>{
+    boton.addEventListener('click',()=>{
+
+    })
+});
+
+let num = Math.random()*9
