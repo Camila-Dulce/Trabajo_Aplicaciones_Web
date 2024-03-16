3e34639404d06b666f488989e41e71e8b81d1593
David Santiago Amaya Reyes <David Santiago Amaya Reyes>
2024-03-16 17:34:49 -0500

    codigo en js: sumar puntos y iniciar tiempo de juego

--- jUEGO_MEMORIA/juego_javascript/index.js
+++ jUEGO_MEMORIA/juego_javascript/index.js
@@ -1,29 +1,37 @@
 let botones = document.getElementsByClassName('seleccionar');
 
 let imageUrls = {
-  1: 'https://i.pinimg.com/originals/09/fe/30/09fe30bee5617384271213308361279c.jpg',
-  2: 'https://i.pinimg.com/originals/a9/8c/ef/a98cef81d9968637981fed0a46fccd9b.png',
-  3: 'https://i.pinimg.com/originals/61/de/ba/61deba51ec625493d1956063bac219fb.jpg'
+  0: 'https://i.pinimg.com/originals/09/fe/30/09fe30bee5617384271213308361279c.jpg',
+  1: 'https://i.pinimg.com/originals/a9/8c/ef/a98cef81d9968637981fed0a46fccd9b.png',
+  2: 'https://i.pinimg.com/originals/61/de/ba/61deba51ec625493d1956063bac219fb.jpg'
 };
 
-let segundos = 0;
 let puntos = 0;
 let comparcion = false;
 let contadorClick = 0;
 let valueAnt = -1;
+let startiempo= false;
  
 function accigarNumeroAleatorio() {
+  let contador = [0, 0, 0]; // Contador para cada número
   for (let i = 0; i < botones.length; i++) {
-    let num = Math.floor(Math.random() * 3); 
-    botones[i].value = num; 
+    let num;
+    do {
+      num = Math.floor(Math.random() * 3); // Genera un número aleatorio del 0 al 2
+    } while (contador[num] >= 2); // Repite si el número ya se ha asignado dos veces
+    botones[i].value = num;
+    contador[num]++;
   }
 };
 accigarNumeroAleatorio();
 
 function buscar(){
-  //let seleccionados = [];
+  let comparcion = false;
+  let contadorClick = 0;
+  let valueAnt = -1;
   [...botones].forEach(boton=>{
     boton.addEventListener('click',(event)=>{
+      startiempo = true;
       const value = boton.getAttribute('value');
       contadorClick++;
       if(contadorClick==1){
@@ -31,12 +39,12 @@ function buscar(){
       }else if(contadorClick==2){
         comparcion = valueAnt==value;
         if(comparcion){
-          //si es verdera sumar puntos
+          sumarPuntos();
+          buscar();
         }else{
-          //reatuarar datos iniciles 
+          buscar();
         }
-      }
-      console.log()
+      }    console.log(value);
     })
   });
 }
@@ -44,17 +52,18 @@ buscar();
 
 
 function sumarPuntos() {
-  // si el boton1 y boton2 sen igual sumar puntos 
-  
+  puntos += 50;
+  document.getElementById('punto').innerText = "puntos"+puntos;
 }
 
-function tiempo() {
-
-  // cuando se realice el primer clik comiense el contador 
-  [...botons].forEach(boton=>{
-    boton.addEventListener('click',()=>{
-
-    })
-  });
+let segundos = 0;
+function incrementarContador() {
+  if (startiempo){
+    segundos++;
+    document.getElementById('tiempo').innerText = "Tiempo"+segundos;
+  }
 }
+const intervalo = 1000; 
+
+setInterval(incrementarContador, intervalo);
 
