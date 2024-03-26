ee2c5a2c0d7e88141eaec42e0fa7d5bdb8d8b4fa
Camila Dulce <mcdulce@uniboyaca.edu.co>
2024-03-26 01:47:25 -0500

    Listo JS agregar y eliminar tareas

--- Gestor_tareas_CRUD/gestor_javascript/index.js
+++ Gestor_tareas_CRUD/gestor_javascript/index.js
@@ -6,6 +6,11 @@ const hecho = "task_alt";
 const pendiente = "radio_button_unchecked";
 const subrayado = "subrayado";
 let cont = 0;
+const listaDatos = [];
+
+//cracion de fecha//
+const fechaActual = new Date ();
+fecha.innerHTML = fechaActual.toLocaleDateString("es-CO", {weekday:"long", month: "short", day:"numeric"});
 
 
 //agregar tarea//
@@ -18,12 +23,15 @@ function agregarTarea (tarea, cont, realizado, eliminado){
     const validacionTarea = realizado ?hecho:pendiente;
     const subr = realizado ?subrayado:"";
 
-    const nuevaLista = '<li id="nuevaLista"><span class="material-symbols-outlined" dato="eliminado" id="'+ cont +'">'+ validacionTarea +'</span><p class="text '+ subr +'">' + tarea + '</p><span class="material-symbols-outlined" dato="eliminado" id="'+ cont +'">delete</span></li>';    
+    const nuevaLista = '<li id="nuevaLista"><span class="material-symbols-outlined" dato="realizado" id="'+ cont +'">'+ validacionTarea +'</span><p class="text '+ subr +'">' + tarea + '</p><span class="material-symbols-outlined" dato="eliminado" id="'+ cont +'">delete</span></li>';    
     lista.innerHTML += nuevaLista;
 
     //evento de click para cambiar estado de tarea//
     const nuevaTarea = lista.querySelectorAll("li");
-    nuevaTarea[nuevaTarea.length-1].addEventListener("click", estado)
+    for (let i = 0; i < nuevaTarea.length; i++) {
+    nuevaTarea[i].addEventListener("click", estado);
+    }
+
 }
 
 //agregar con boton//
@@ -31,39 +39,84 @@ botonAgregar.addEventListener("click", () => {
     const tarea = input.value;
     if (tarea) {
         agregarTarea(tarea, cont, false, false);
+        //agregar datos al array//
+        listaDatos.push({
+            nombre: tarea,
+            cont: cont,
+            realizado: false,
+            eliminado:false,
+        })
         input.value = "";
         cont++;
     } else {
-        console.log("Ingresa una tarea valida");
+        const error = "Ingresa una tarea válida";
+        mensajeError(error);
     }
 });
+
+//mensaje tarea invalida//
+function mensajeError (error){
+    const mensaje = document.createElement("p");
+    mensaje.textContent = error;
+    mensaje.classList.add("error");
+    const errorCont = document.querySelector(".mensajeError"); 
+    errorCont.innerHTML = '';
+    errorCont.appendChild(mensaje);
+
+    //duracion de mensaje de error//
+    setTimeout(function() {
+        errorCont.innerHTML = ''; 
+    }, 3000);
+}
 
 //agregar con enter//
 document.addEventListener("keyup", function(tar){
     if(tar.key=="Enter"){
         const tarea = input.value;
         if (tarea) {
-            agregarTarea(tarea, id, false, false);
+            agregarTarea(tarea, cont, false, false);
+            //agregar datos al array//
+        listaDatos.push({
+            nombre: tarea,
+            cont: cont,
+            realizado: false,
+            eliminado:false,
+        })
             input.value = "";
             cont++
         } else {
-            console.log("Ingresa una tarea valida");
+            const error = "Ingresa una tarea válida";
+            mensajeError(error);
         }
     }
 });
 
-//marcar hecho o pendiente
-
+//marcar hecho o pendiente//
 function estado(tar){
     const tarea = tar.target;
 
     if (tarea.tagName === "SPAN"){
-        if (tarea.textContenido === pendiente){
-            tarea.textContenido = hecho;
-        } else if (elemento.textContent === hecho) {
-            elemento.textContent = pendiente;
+        if (tarea.textContent === pendiente){
+            tarea.textContent = hecho;
+            tarea.nextElementSibling.classList.add(subrayado);
+            listaDatos[tarea.id].realizado = listaDatos[tarea.id].realizado ?false :true;
+        } else if (tarea.textContent === hecho) {
+            tarea.textContent = pendiente;
+            tarea.nextElementSibling.classList.remove(subrayado); 
         }
     }
 }
 
-
+//eliminar tarea//
+function eliminarTarea(tar) {
+    if (tar.target.getAttribute("dato") === "eliminado") {
+      tar.target.parentElement.remove();
+      if (listaDatos[tar.id]) {
+        listaDatos[tar.id].eliminado = true;
+      } else {
+        console.error("Elemento eliminado");
+      }
+    }
+  }
+  
+  lista.addEventListener("click", eliminarTarea);
